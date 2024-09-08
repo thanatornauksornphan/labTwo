58fcc37f193866323b0395885f1ee19800bfebce
Thanatorn Auksornphan <thanatornauksornphan@gmail.com>
2024-09-08 15:40:17 +0700

    updated register routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,15 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent},
+    { path: 'register', component: RegisterComponent},
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule{}
