d7c11b1da396eebacc534ebfbfa1d13d63f54833
Thanatorn Auksornphan <thanatornauksornphan@gmail.com>
2024-09-08 15:38:40 +0700

    updated routes

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,12 @@ import { provideRouter } from '@angular/router';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
 const mergedAppConfig = {
   ...appConfig,
   providers: [
     ...(appConfig.providers || []),
     provideRouter(routes),
-    provideAnimations(),
   ]
 }
 
